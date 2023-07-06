9db64035cdb0233e01767474903dacfcd648d7f0
Rajat Mehta <mehtarajat1772002@gmail.com>
2023-07-06 16:46:47 +0530

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 export default function Home() {
-  return <Box></Box>;
+  return (
+    <Box>
+      <Typography> Click on Get Users button to get users</Typography>
+    </Box>
+  );
 }
